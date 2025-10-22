4aefebb5f86a4066d7d24dbcb0250f9f3a35da47
David Salvador Castellanos <david.salvador.castellanos.dsc@gmail.com>
2025-10-22 11:25:23 +0200

    Ejercicio 2 Repaso

--- app/src/main/java/com/example/repasoplayjuegos/MainActivity.kt
+++ app/src/main/java/com/example/repasoplayjuegos/MainActivity.kt
@@ -2,14 +2,20 @@ package com.example.repasoplayjuegos
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
 
         val jugador = findViewById<Button>(R.id.button2)
         jugador.setOnClickListener { lanzarNuevoJugador() }
@@ -19,4 +25,19 @@ class MainActivity : AppCompatActivity() {
         val i = Intent(this, NewPlayer::class.java)
         startActivity(i)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+//        return when (item.itemId) {
+//            R.id.action_buscar -> {
+//                lanzarSearch()
+//                true
+//            }
+//            else -> super.onOptionsItemSelected(item)
+//        }
+//    }
 }
--- app/src/main/java/com/example/repasoplayjuegos/NewPlayer.kt
+++ app/src/main/java/com/example/repasoplayjuegos/NewPlayer.kt
@@ -1,14 +1,23 @@
 package com.example.repasoplayjuegos
 
 import android.os.Bundle
+import android.view.Menu
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 
 class NewPlayer : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.newplayer)
 
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
     }
 }
